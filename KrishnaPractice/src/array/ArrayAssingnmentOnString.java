15686c50491b86ce1408a1ae1548a9e29039f922
krishnapl123 <krishnapl.xworkz@gmail.com>
2021-11-11 20:03:52 +0530

    Single Dimensional Array Assignment

---
+++ KrishnaPractice/src/array/ArrayAssingnmentOnString.java
@@ -0,0 +1,32 @@
+package array;
+
+public class ArrayAssingnmentOnString {
+	
+	public static void main(String args[]) {
+		
+	
+		
+		String[] strarr= {"apple","banana","mango","kiwi","fig"};
+
+		
+		for(int i=0;i<strarr.length;i++) {
+			
+			if(strarr[i].length()<5)
+			{
+			
+			System.out.println(strarr[i]);
+			}
+			
+			
+				
+				
+			}
+	}
+}
+
+			
+		
+		
+	
+
+
---
+++ KrishnaPractice/src/array/single_dimensional_array/SingleDimensionalArrayDemo.java
@@ -0,0 +1,26 @@
+package array.single_dimensional_array;
+
+public class SingleDimensionalArrayDemo {
+	
+	public static void main(String args[]) {
+		
+		//int[] arr=new int[5];
+		
+		int[] arr= {1,2,3,4,5};
+		
+		//int num=1;		
+		for(int i=0;i<arr.length;i++) {
+			
+			//arr[i]=num;
+			System.out.println(arr[i]);
+		//	num++;
+		}
+		
+		//for(int i=0;i<arr.length;i++) {
+			
+			//System.out.println(arr[i]);
+			
+		//}
+	}
+
+}
