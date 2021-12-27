014d6b40795576b256c661fa565e0f9ada64ac9c
krishnapl123 <krishnapl.xworkz@gmail.com>
2021-12-27 15:40:19 +0530

    second question on ItemType

---
+++ KrishnaPractice/src/collection_framework/collections_Problem_Statements/ItemType.java
@@ -0,0 +1,60 @@
+package collection_framework.collections_Problem_Statements;
+
+public class ItemType {
+	
+	private String name;
+	private double deposit;
+	private double costperDay;
+	
+	
+	public ItemType() {
+		
+	}
+
+
+	@Override
+	public String toString() {
+		return String.format("%-20s%-20s%-20s%", name, deposit, costperDay);
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getDeposit() {
+		return deposit;
+	}
+
+
+	public void setDeposit(double deposit) {
+		this.deposit = deposit;
+	}
+
+
+	public double getCostperDay() {
+		return costperDay;
+	}
+
+
+	public void setCostperDay(double costperDay) {
+		this.costperDay = costperDay;
+	}
+
+
+	public ItemType(String name, double deposit, double costperDay) {
+		super();
+		this.name = name;
+		this.deposit = deposit;
+		this.costperDay = costperDay;
+	}
+	
+	
+
+}
---
+++ KrishnaPractice/src/collection_framework/collections_Problem_Statements/ItemTypeMain.java
@@ -0,0 +1,46 @@
+package collection_framework.collections_Problem_Statements;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class ItemTypeMain {
+	
+	public static void main(String args[]) {
+		
+		Collection<ItemType> collection=new ArrayList<>();
+		
+		Scanner sc=new Scanner(System.in);
+		Scanner scInt=new Scanner(System.in);
+		int n=1;
+		String str="";
+		
+		do {
+			
+			System.out.println("Enter the details of Type "+n++);
+			System.out.println("Name:");
+			String name=sc.next();
+			System.out.println("Deposit:");
+			double deposit=scInt.nextInt();
+			System.out.println("CostPerDay:");
+			double CostperDay=scInt.nextInt();
+			System.out.println("Do you wnat to continue?y/n");
+		        str=sc.next();
+			
+		}while("y".equals(str));
+		
+		Iterator<ItemType> itr=collection.iterator();
+		System.out.printf("%-20s%-20.1f%-20.1f", "name", "deposit", "costperDay");
+		System.out.println();
+		
+		while(itr.hasNext()) {
+			ItemType itemType=itr.next();
+			System.out.println(itemType);
+		}
+		
+		
+		
+	}
+
+}
